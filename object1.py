afb23613489d5c858b52992b9ecba7937ec187e6
karanjanyaga93 <79145474+karanjanyaga93@users.noreply.github.com>
2021-02-25 13:33:10 +0300

    Add files via upload

---
+++ object1.py
@@ -0,0 +1,16 @@
+class zoo:
+    def _init_(self,cat,male,canivore):
+        self.cat=cat
+        self.male=male
+        self.carnivore=carnivore
+    def getzoo(self):
+        return self.cat
+    def getzoo(self) :  
+        return self.male
+    def getzoo(self) :
+        return self.carnivore
+lion=cat
+lion1=zoo("cat","male","carnivore")  
+print(lion1.getcat())  
+print(lion1.getmale())
+print(lion1.carnivore())
